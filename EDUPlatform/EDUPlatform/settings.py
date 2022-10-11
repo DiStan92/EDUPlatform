bcb4b7f74667e2a48e5332f7bd41655da2f3d796
Nikita <nikitapt63@gmail.com>
2022-10-11 20:10:12 +0300

    Linter changes

--- EDUPlatform/EDUPlatform/settings.py
+++ EDUPlatform/EDUPlatform/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 
 # Import environment variables
 load_dotenv()
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
