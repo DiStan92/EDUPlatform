ad963a2ee3ef0210a21bf73424e60a788ce23233
Nikita <nikitapt63@gmail.com>
2022-10-18 20:00:51 +0300

    Linter changes

--- EDUPlatform/users/admin.py
+++ EDUPlatform/users/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Student, Teacher, User
 
-
 admin.site.register(User)
 admin.site.register(Teacher)
 admin.site.register(Student)
--- EDUPlatform/users/migrations/0001_create_user_model.py
+++ EDUPlatform/users/migrations/0001_create_user_model.py
@@ -27,9 +27,7 @@ class Migration(migrations.Migration):
                 ("password", models.CharField(max_length=128, verbose_name="password")),
                 (
                     "last_login",
-                    models.DateTimeField(
-                        blank=True, null=True, verbose_name="last login"
-                    ),
+                    models.DateTimeField(blank=True, null=True, verbose_name="last login"),
                 ),
                 (
                     "is_superuser",
@@ -41,21 +39,15 @@ class Migration(migrations.Migration):
                 ),
                 (
                     "first_name",
-                    models.CharField(
-                        blank=True, max_length=150, verbose_name="first name"
-                    ),
+                    models.CharField(blank=True, max_length=150, verbose_name="first name"),
                 ),
                 (
                     "last_name",
-                    models.CharField(
-                        blank=True, max_length=150, verbose_name="last name"
-                    ),
+                    models.CharField(blank=True, max_length=150, verbose_name="last name"),
                 ),
                 (
                     "email",
-                    models.EmailField(
-                        max_length=254, unique=True, verbose_name="email address"
-                    ),
+                    models.EmailField(max_length=254, unique=True, verbose_name="email address"),
                 ),
                 (
                     "is_staff",
